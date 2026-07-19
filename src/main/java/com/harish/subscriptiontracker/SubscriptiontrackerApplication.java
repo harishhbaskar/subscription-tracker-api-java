5c0ec09eeb2be6d1702edb18b6e363d1d7de1124
harishhbaskar <harishhbaskar@gmail.com>
2026-07-19 23:01:10 +0530

    Initialize Spring Boot project with Maven

---
+++ src/main/java/com/harish/subscriptiontracker/SubscriptiontrackerApplication.java
@@ -0,0 +1,13 @@
+package com.harish.subscriptiontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptiontrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptiontrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harish/subscriptiontracker/SubscriptiontrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harish.subscriptiontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptiontrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
